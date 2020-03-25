62493855715a1710b6857591fede3dcd28be6aef
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-03-25 12:25:52 -0500

    cleaned up code

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     var firstNumber = "";
     var operator = "";
@@ -12,8 +12,8 @@ $(document).ready(function() {
     // How will you differentiate numbers from operators? value attr
     // How will you know the "value" of a number clicked? 
     // How will you know when a user is done entering the first number? operator
-    
-    $(".btn").on("click", function(event) {
+
+    $(".btn").on("click", function (event) {
         let targetValue = $(this).attr("value");
         let targetText = $(this).text();
 
@@ -43,21 +43,21 @@ $(document).ready(function() {
         var number1 = parseInt(firstNumber)
         var number2 = parseInt(secondNumber)
         switch (operator) {
-        case "plus":
-            total = number1 + number2;
-            break;
-    case "minus":
-    total = number1 - number2;
-    break;
-case "times":
-    total = number1 * number2;
-    break;
-case "divide":
-    total = number1 / number2;
-    break;
-case "power":
-    total = number1 ^ number2;
-    break;
-
+            case "plus":
+                total = number1 + number2;
+                break;
+            case "minus":
+                total = number1 - number2;
+                break;
+            case "times":
+                total = number1 * number2;
+                break;
+            case "divide":
+                total = number1 / number2;
+                break;
+            case "power":
+                total = number1 ^ number2;
+                break;
+        }
+    }
 });
-    
